72822ec6d1007b66c23a9e2d59d72fed56802b39
Krystian Cichorz <krystiancichorz708@gmail.com>
2023-04-01 23:44:40 +0200

    1.1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="flex text-center justify-center h-[25px] text-neutral-100 bg-neutral-700">
-      <h4>Made by Krys &copy;</h4>
+      <h4>Made by Krys&copy;</h4>
     </div>
   );
 };
